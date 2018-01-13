378812c871072f056d47d4c1d029c42e97572365
Jorge Gómez <jgomez@fonetic.local>
2018-01-13 15:44:07 +0100

    Extract fizzBuzzer results by divisor to a dictionary

--- FizzBuzz/FizzBuzzer.cs
+++ FizzBuzz/FizzBuzzer.cs
@@ -1,19 +1,26 @@
 ﻿using FizzBuzz.Extensions;
+using System.Collections.Generic;
 using System.Text;
 
 namespace FizzBuzz
 {
     public class FizzBuzzer : IFizzBuzzer
     {
+        private readonly IDictionary<int, string> _resultByDivisor = new Dictionary<int, string>()
+        {
+            [3] = "Fizz",
+            [5] = "Buzz",
+        };
+
         public string GetNumberFizzBuzzed(int number)
         {
             var stringBuilder = new StringBuilder();
 
-            if (number.IsDivisibleBy(3))
-                stringBuilder.Append("Fizz");
-
-            if (number.IsDivisibleBy(5))
-                stringBuilder.Append("Buzz");
+            foreach (var divisor in _resultByDivisor.Keys)
+            {
+                if (number.IsDivisibleBy(divisor))
+                    stringBuilder.Append(_resultByDivisor[divisor]);
+            }
             
             return stringBuilder.IsEmpty()
                 ? number.ToString() 
